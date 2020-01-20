4514e5f1d6dfe618fc2f53503f9a3c9061e16230
Johnathon Hutt <johnathonhutt@gmail.com>
2020-01-20 14:31:13 -0500

    Add CORS

--- app.js
+++ app.js
@@ -3,10 +3,15 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 const futurama = require("./futuramaQuotes");
 
-//console.log(futurama.newQuote());
+//use cors
+app.use(cors());
+
+//allow OPTIONS on all resources
+app.options('*', cors());
 
 app.get('/', function (req, res) {
   res.send("For Futurama quotes, visit '/futurama'");
